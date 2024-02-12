c353c50c5de62cffac2c4abdfc26bcb6631968c9
ArkaiduszKapa <146595423+ArkaiduszKapa@users.noreply.github.com>
2024-02-12 12:45:28 +0100

    Add files via upload
    correct file

--- BlooGame-alpha/script.js
+++ BlooGame-alpha/script.js
@@ -1,3 +1,5 @@
+
+
 const levels = [
   {
     id: 1,
@@ -6,59 +8,34 @@ const levels = [
       lighting: 'Dim',
       atmosphere: 'Eerie',
     },
-    regions: [
-      { id: 1, name: 'tower', mobs: [] },
-      { 
-        id: 2, 
-        name: 'dark entrance', 
-        mobs: [
-          { name: 'slime', lvl: 2, exp: 3, dmg: 5, hp: 10 },
-          { name: 'demonic_slime', lvl: 5, exp: 12, dmg: 10, hp: 20 },
-        ],
-      },
-      { 
-        id: 3, 
-        name: 'forgotten hallway',
-        mobs: [
-          { name: 'slime', lvl: 2, exp: 3, dmg: 5, hp: 10 },
-          { name: 'demonic_slime', lvl: 5, exp: 12, dmg: 10, hp: 20 },
-        ],
-      },
-    ],
-    mission: 'Find the lost artifact',
-    clearingRequirement: 'Defeat all mobs',
+    regions: [{ id: 1, name: 'tower', mobs: [] }],
+    mission: 'Survive in the Tower!',
+    clearingRequirement: 'None!',
   },
   {
     id: 2,
     name: 'Enchanted Forest',
     settings: {
-      lighting: 'Glowing',
       atmosphere: 'Mystical',
     },
     regions: [
       { id: 1, name: 'tower', mobs: [] },
-      { 
-        id: 2, 
-        name: 'dark entrance', 
-        mobs: [
-          { name: 'slime', lvl: 2, exp: 3, dmg: 5, hp: 10 },
-          { name: 'demonic_slime', lvl: 5, exp: 12, dmg: 10, hp: 20 },
-        ],
+      {
+        id: 2,
+        name: 'dark entrance',
+        mobs: [{ name: 'slime', lvl: 2, exp: 3, dmg: 3, hp: 10 ,max_hp:10, money:3 }],
       },
-      { 
-        id: 3, 
-        name: 'forgotten hallway',
-        mobs: [
-          { name: 'slime', lvl: 2, exp: 3, dmg: 5, hp: 10 },
-          { name: 'demonic_slime', lvl: 5, exp: 12, dmg: 10, hp: 20 },
-        ],
+      {
+        id: 3,
+        name: 'forgotten cave',
+        mobs: [{ name: 'demonic_slime', lvl: 8, exp: 13, dmg: 10, hp: 35,max_hp:35, money:11  }],
       },
     ],
-    mission: 'Protect the mystical artifact',
-    clearingRequirement: 'Defeat all mobs',
+    mission: 'Fight monsters',
+    clearingRequirement: 'Get to tutorial level 30',
   },
-
 ];
+
 
 class Player {
   constructor(name, level, hp, maxHp, hunger, maxHunger, xp, maxXp, dmg, money) {
@@ -72,6 +49,7 @@ class Player {
     this.maxXp = maxXp || this.calculateMaxXp();
     this.dmg = dmg || 2;
     this.money = money || 0;
+    this.textContent = new TextBasedContent();
   }
 
   calculateMaxHp() {
@@ -88,6 +66,11 @@ class Player {
     this.hp = this.maxHp;
     this.maxXp = this.calculateMaxXp();
     this.xp = 0;
+    if (this.level == 30){
+      myGame.stop();
+    }
+
+    
   }
 
   takeDamage(amount) {
@@ -106,17 +89,6 @@ class Player {
     this.xp = 0;
   }
 
-  attack(mob) {
-    mob.hp -= this.dmg;
-    if (mob.hp <= 0) {
-      this.xp += mob.exp;
-      if (this.xp >= this.maxXp) {
-        this.levelUp();
-      }
-      mob.hp = 0;
-    }
-  }
-
   updateStats() {
     this.maxHp = this.calculateMaxHp();
     this.maxXp = this.calculateMaxXp();
@@ -132,60 +104,29 @@ class Commands {
   }
 
   ChangeLevel(desiredLevel) {
-      this.navigation.navigateLevels(desiredLevel);
+    this.navigation.navigateLevels(desiredLevel);
+    this.experience.setCurrentRegion(this.navigation.getCurrentRegion());
   }
 
   ChangeRegion(desiredRegion) {
-      const region = this.navigation.findRegionByName(desiredRegion);
-      if (region) {
-        this.actionInProgress = true;
-        this.navigation.navigateRegions(desiredRegion);
-        this.experience.setCurrentRegion(region);
-        this.navigation.displayCurrentMap();
-        this.actionInProgress = false;
-      } else {
-        console.log('Invalid region name.');
-      }
-    }
+    this.navigation.navigateRegions(desiredRegion);
+    this.experience.setCurrentRegion(this.navigation.getCurrentRegion());
+  }
 
   pve(mobName, attackCount) {
-      const currentRegion = this.navigation.getCurrentRegion();
-      if (currentRegion) {
-        this.actionInProgress = true;
-        this.experience.setCurrentRegion(currentRegion);
-        const attackedMob = this.experience.findMob(mobName);
-
-        if (attackedMob) {
-          for (let i = 0; i < attackCount; i++) {
-            console.log(`Attacked ${mobName} in ${currentRegion.name}! Mob details:`, attackedMob);
-
-            const xpGain = this.experience.calculateXpGain(attackedMob);
-            this.player.xp += xpGain;
-
-            if (this.player.xp >= this.player.calculateMaxXp()) {
-              this.player.levelUp();
-            }
-
-            this.experience.updateUI();
-          }
-        } else {
-          console.log(`${mobName} not found in ${currentRegion.name}.`);
-        }
-        this.actionInProgress = false;
-      } else {
-        console.log('No current region.');
-      }
+    this.experience.pve(mobName, attackCount);
   }
 }
 
 class Experience {
   constructor(player, xp_bar_fill, lvlElement, navigation) {
     this.player = player;
-    this.currentRegion = null;
     this.xp_bar_fill = xp_bar_fill;
     this.lvlElement = lvlElement;
     this.navigation = navigation; 
+    this.currentRegion = this.navigation.getCurrentRegion();
     this.updateUI();
+    this.textContent = new TextBasedContent();
   }
 
   setCurrentRegion(region) {
@@ -193,16 +134,10 @@ class Experience {
     this.updateUI();
   }
 
-  levelUp() {
-    this.player.xp = 0;
-    this.player.level += 1;
-    this.updateUI();
-  }
-
   calculateXpGain(mob) {
     const baseXp = mob.exp;
     const levelDifference = mob.lvl - this.player.level;
-    const adjustedXp = Math.max(0, baseXp - levelDifference * 5);
+    const adjustedXp = Math.max(0, baseXp + levelDifference);
     return adjustedXp;
   }
 
@@ -210,19 +145,29 @@ class Experience {
     const attackedMob = this.findMob(mobName);
     if (attackedMob) {
       for (let i = 0; i < attackCount; i++) {
-        if (this.player.xp >= this.player.calculateMaxXp()) {
-          this.player.levelUp();
-        } else {
-          const xpGain = this.calculateXpGain(attackedMob);
-          this.player.xp += xpGain;
-        }
+        this.attack(mobName, attackedMob);
+        this.updateUI();
       }
+      console.log(`${mobName} attacked in ${this.currentRegion.name}.`);
+      this.textContent.createMessage('?????', 'speaker', 'WorldVoice', `[${mobName} attacked in ${this.currentRegion.name}]`);
       this.updateUI();
     } else {
       console.log(`${mobName} not found in ${this.currentRegion.name}.`);
+      this.textContent.createMessage('?????', 'speaker', 'WorldVoice', `[${mobName} not found in ${this.currentRegion.name}]`);
     }
   }
 
+  attack(mobName, mob) {
+    mob.hp -= this.player.dmg;
+    if (mob.hp <= 0) {
+      this.player.xp += this.calculateXpGain(mob);
+      this.player.money += mob.money;
+      if (this.player.xp >= this.player.maxXp) {
+        this.player.levelUp();
+      }
+      mob.hp = 0;
+    }
+  }
   updateUI() {
     if (this.xp_bar_fill) {
       this.xp_bar_fill.style.width = (this.player.xp / this.player.calculateMaxXp()) * 100 + "%";
@@ -233,18 +178,28 @@ class Experience {
   }
 
   findMob(mobName) {
-    const mobs = this.currentRegion.mobs;
-    return mobs.find(mob => mob.name === mobName);
+    const currentRegion = this.navigation.getCurrentRegion();
+    const mobs = currentRegion.mobs || [];
+    return mobs.find((mob) => mob.name === mobName);
   }
 }
 
 class MapNavigation {
-  constructor() {
+  constructor(player) {
+    this.player = player;
     this.levels = [];
     this.currentLevel = null;
-    this.currentRegion = null;
-  }
+    this.currentRegion = 'tower';
+    this.textContent = new TextBasedContent();
 
+    levels.forEach((level) => {
+      level.regions.forEach((region) => {
+        if (!region.mobs) {
+          region.mobs = [];
+        }
+      });
+    });
+  }
   createMap(levels) {
     this.levels = levels;
     console.log('Map created!');
@@ -255,13 +210,16 @@ class MapNavigation {
     if (region) {
       this.currentRegion = region;
       console.log(`Entered region: ${this.currentRegion.name}`);
-      this.displayCurrentMap();
+      this.textContent.createMessage('?????', 'speaker', 'WorldVoice', `[Entered region: ${this.currentRegion.name}]`);
     } else {
       console.log('Invalid region name.');
+      this.textContent.createMessage('?????', 'speaker', 'WorldVoice', '[There is no region like that]');
+      
     }
   }
 
-  navigateLevels(levelId) {
+navigateLevels(levelId) {
+  if(this.currentRegion =='tower'){
     const level = this.findLevelById(levelId);
 
     if (level) {
@@ -270,19 +228,24 @@ class MapNavigation {
       this.displayCurrentMap();
     } else {
       console.log('Invalid level ID.');
+      this.textContent.createMessage('?????', 'speaker', 'WorldVoice', '[There is no floor like that]');
     }
+  }else{
+    this.textContent.createMessage('?????', 'speaker', 'WorldVoice', '[You need to be in the region: (tower) in order to transmit to anotcher level]');
   }
+}
 
   displayCurrentMap() {
-    console.log(`You are in Room ${this.currentLevel.id}.`);
-    console.log(`This room has doors leading to regions: ${this.currentLevel.regions.map(region => region.name).join(', ')}`);
+    this.textContent.createMessage('?????', 'speaker', 'WorldVoice', `[You are in floor ${this.currentLevel.id}]`);
+    this.textContent.createMessage('?????', 'speaker', 'WorldVoice', `[This room has regions:  ${this.currentLevel.regions.map(region => region.name).join(' , ')}]`);
   }
 
   findRegionByName(regionName) {
     const foundRegion = this.levels.reduce((foundRegion, level) => {
-      const region = level.regions.find(region => region.name === regionName);
+      const region = level.regions.find((region) => region.name === regionName);
       return foundRegion || region;
     }, null);
+
     return foundRegion || null;
   }
 
@@ -340,12 +303,12 @@ class TextBasedContent {
 class Game {
   constructor() {
 
-    this.player = new Player('Kolczak', 1);
+    this.player = new Player();
     this.commands = new Commands(this.player);
     this.inputField = document.getElementById('player_text_input');
     this.playerInputValue = '';
     this.textContent = new TextBasedContent();
-    this.map = new MapNavigation();
+    this.map = new MapNavigation(this.player);
 
     this.inputField.addEventListener('keydown', (event) => {
       if (event.key === 'Enter') {
@@ -356,38 +319,48 @@ class Game {
       }
     });
 
+
     this.updatePlayerStats();
   }
 
   start(){
-    this.textContent.createMessage('World', 'speaker', 'WorldVoice', 'Your game has started!');
+    this.gameStarted = true;
+    this.textContent.createMessage('?????', 'speaker', 'WorldVoice', '[Your game has started!]');
+    this.textContent.createMessage('?????', 'speaker', 'WorldVoice', '[Use the cheat sheet on left to transfer to 2nd floor and go fight slimes in its regions!]');
   }
+  stop(){
+    this.gameStarted = false;
+    this.textContent.createMessage('?????', 'speaker', 'WorldVoice', '[Your game has ended!]');
 
+  }
   PlayerChose() {
-    let inputField = document.getElementById('player_text_input');
-    let string = inputField.value.trim().toLowerCase();
-    console.log('Input:', string);
-    let matchLevel = string.match(/transmit to floor (\d+)/);
-    let matchRegion = string.match(/\bgo to the region\b(.+)/);
-    let matchAttack = string.match(/attack\s+([\w_]+)(?:\s+x(\d+))?/);
-    if (matchLevel) {
-      let destination = parseInt(matchLevel[1], 10);
-      console.log('Level ID:', destination);
-      this.commands.ChangeLevel(destination);
-    } else if (matchRegion) {
-      let destination = matchRegion[1].trim();
-      console.log("adasdasd " + destination);
-      this.commands.ChangeRegion(destination);
-    } else if (matchAttack) {
-      const mobName = matchAttack[1];
-      const attackCount = matchAttack[2] ? parseInt(matchAttack[2], 10) : 1;
-      this.commands.pve(mobName, attackCount);
-    } else if (string.includes('use item')) {
-      console.log('Player wants to use an item!');
-    } else {
-      console.log('Unrecognized command:', string);
+    if(this.gameStarted){
+      let inputField = document.getElementById('player_text_input');
+      let string = inputField.value.trim().toLowerCase();
+      console.log('Input:', string);
+      let matchLevel = string.match(/transmit to floor (\d+)/);
+      let matchRegion = string.match(/\bgo to region\b(.+)/);
+      let matchAttack = string.match(/attack\s+([\w_]+)(?:\s+x(\d+))?/);
+      if (matchLevel) {
+        let destination = parseInt(matchLevel[1], 10);
+        console.log('Level ID:', destination);
+        this.commands.ChangeLevel(destination);
+      } else if (matchRegion) {
+        let destination = matchRegion[1].trim();
+        this.commands.ChangeRegion(destination);
+      } else if (matchAttack) {
+        const mobName = matchAttack[1];
+        const attackCount = matchAttack[2] ? parseInt(matchAttack[2], 10) : 1;
+        this.commands.pve(mobName, attackCount);
+      } else if (string.includes('use item')) {
+        console.log('Player wants to use an item!');
+      } else {
+        console.log('Unrecognized command:', string);
+      }
+      this.updatePlayerStats();
+    }else{
+      this.textContent.createMessage('World', 'speaker', 'WorldVoice', '[Your game is not started!]');
     }
-    this.updatePlayerStats();
   }
 
   updatePlayerStats() {
@@ -407,6 +380,7 @@ class Game {
     money.innerHTML = this.player.money;
   }
 }
+
 
 const myGame = new Game();
 myGame.start();
